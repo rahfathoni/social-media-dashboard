ab3fd11b34515a9b5f2e730633a60a4386007215
rahfathoni <rahfathoni@gmail.com>
2024-04-20 23:34:40 +0700

    develop layout; develop list user page

---
+++ app/list-user/page.js
@@ -0,0 +1,15 @@
+export const metadata = {
+    title: 'List User'
+};
+
+export default function Page() {
+    return (
+        <div className="w-full">
+            <header>
+                <h1 className="text-2xl">
+                    List User
+                </h1>
+            </header>
+        </div>
+    )
+}
