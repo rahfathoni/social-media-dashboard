cbc085e8577de89b19b81f240794f27946369593
rahfathoni <rahfathoni@gmail.com>
2024-04-22 08:57:37 +0700

    profile: list user albums

--- app/dashboard/list-user/[id]/profile/album.jsx
+++ app/dashboard/list-user/[id]/profile/album.jsx
@@ -1,8 +1,22 @@
-export default function UserAlbum({data}) {
+export default function UserAlbum({ data }) {
   return (
-    <div>
-      album user
-      {JSON.stringify(data)}
-    </div>
-  )
+    <section className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+      {data.map((album, index) => (
+        <div key={index} className="bg-gray-900 shadow-lg rounded p-3">
+          <div className="group relative">
+            <img className="w-full md:w-72 block rounded" src="/no-image.png" alt={album.title} />
+            <div className="absolute bg-black rounded bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition justify-evenly">
+              <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                See Pictures
+              </button>
+            </div>
+          </div>
+          <div className="p-5">
+            <h3 className="text-white text-lg">{album.title}</h3>
+            <p className="text-gray-400">{album.artist}</p>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 }
